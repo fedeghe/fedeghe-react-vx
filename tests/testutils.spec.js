00882ba6e17291d36b4a20743e4c4b793a7045f5
fedeghe <fedeghe@gmail.com>
2022-12-07 08:40:48 +0100

    moana forever

--- tests/testutils.spec.js
+++ tests/testutils.spec.js
@@ -1,5 +1,5 @@
 
-import { generateRowData, getConfig } from './utils'
+import { generateRowData, getConfig } from './utils';
 
 describe('test utils wor as expected', function () {
     it('returns the expected - generateRowData', () => {
@@ -9,8 +9,8 @@ describe('test utils wor as expected', function () {
             { key: 'd', type: 'date'},
             { key: 'i', type: 'id'},
             { key: 'c', type: 'company'},
-        ], 50)
-        expect (data.length).toBe(50)
+        ], 50);
+        expect (data.length).toBe(50);
     });
 
     it('returns the expected - getConfig', () => {
@@ -19,7 +19,7 @@ describe('test utils wor as expected', function () {
             },
             cnf2 = getConfig(cnf1);
         cnf1.one[0].a = 2;
-        expect(cnf1.one[0].a).toBe(2)
-        expect(cnf2.one[0].a).toBe(1)
+        expect(cnf1.one[0].a).toBe(2);
+        expect(cnf2.one[0].a).toBe(1);
     });
 });
